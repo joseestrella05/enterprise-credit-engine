feb4d55515feb43462d4096ef33a5d373159a03b
jestrella <josegabrielestrella04@gmail.com>
2026-08-27 10:50:59 -0400

    refactor: simplify MoneyTests by removing unused tests and consolidating assertions

--- tests/Ecre.Domain.Tests/Common/MoneyTests.cs
+++ tests/Ecre.Domain.Tests/Common/MoneyTests.cs
@@ -1,46 +1,27 @@
-namespace Ecre.Domain.Tests.Common;
-
 using Ecre.Domain.Common;
 using FluentAssertions;
-using Xunit;
+
+namespace Ecre.Domain.Tests.Common;
 
 public class MoneyTests
 {
     [Fact]
-    public void Of_WithUndefinedCurrency_ShouldThrowDomainException()
+    public void Of_AplicaRedondeoComercial_NoBankers()
     {
-        var action = () => Money.Of(100m, default);
-        action.Should().Throw<DomainException>()
-              .WithMessage("*sin moneda definida*");
-    }
-
-    [Theory]
-    [InlineData(10.555, 10.56)] // Redondeo comercial: 5 hacia arriba
-    [InlineData(10.554, 10.55)]
-    [InlineData(2.505, 2.51)]
-    public void Of_ShouldApplyCommercialRounding(decimal input, decimal expected)
-    {
-        var money = Money.Of(input, Currency.DOP);
-        money.Amount.Should().Be(expected);
+        Money.Of(2.005m, Currency.DOP).Amount.Should().Be(2.01m);
+        Money.Of(2.015m, Currency.DOP).Amount.Should().Be(2.02m);
     }
 
     [Fact]
-    public void Addition_WithDifferentCurrencies_ShouldThrowDomainException()
+    public void Operaciones_EntreMonedasDistintas_Fallan()
     {
-        var dop = Money.Of(100m, Currency.DOP);
-        var usd = Money.Of(100m, Currency.USD);
-
-        var action = () => { var _ = dop + usd; };
-        action.Should().Throw<DomainException>()
-              .WithMessage("*monedas distintas*");
+        var accion = () => Money.Of(100m, Currency.DOP) + Money.Of(100m, Currency.USD);
+        accion.Should().Throw<DomainException>();
     }
 
     [Fact]
-    public void Min_ShouldReturnLesserAmount()
+    public void Pow_MantienePrecisionDecimal()
     {
-        var m1 = Money.Of(50m, Currency.DOP);
-        var m2 = Money.Of(100m, Currency.DOP);
-
-        Money.Min(m1, m2).Should().Be(m1);
+        DecimalMath.Pow(1.02m, 3).Should().Be(1.061208m);
     }
 }
