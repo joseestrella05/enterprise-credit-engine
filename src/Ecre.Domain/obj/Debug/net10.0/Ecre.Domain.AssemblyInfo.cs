39df958654172b0aace1a67df6ce525148513243
jestrella <josegabrielestrella04@gmail.com>
2026-08-27 12:57:34 -0400

    feat: implement JournalEntry class with debit and credit methods; update assembly info version

--- src/Ecre.Domain/obj/Debug/net10.0/Ecre.Domain.AssemblyInfo.cs
+++ src/Ecre.Domain/obj/Debug/net10.0/Ecre.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecre.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd636032fb53fd9e6844fcaa7925746780e8118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450be8af0c7b3765ae1fb4ffa400c05d11467f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecre.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecre.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
