bb8d1f35c1f9db9660202006e3e89b86bad70d15
jestrella <josegabrielestrella04@gmail.com>
2026-08-27 10:43:40 -0400

    feat: add PaymentFrequency enum for payment scheduling options

---
+++ src/Ecre.Domain/Common/PaymentFrequency.cs
@@ -0,0 +1,10 @@
+namespace Ecre.Domain.Common;
+
+public enum PaymentFrequency
+{
+    Monthly     = 12,
+    Semimonthly = 24, 
+    Biweekly    = 26, 
+    Weekly      = 52,
+    Quarterly   = 4
+}
